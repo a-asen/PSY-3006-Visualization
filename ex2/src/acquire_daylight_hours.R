a75658039df6030b705d4f61fdd9501bd3afcbed
Steffen Aasen <98240487+a-asen@users.noreply.github.com>
2023-10-02 21:02:16 +0200

    weird date fix (1970)

--- ex2/src/acquire_daylight_hours.R
+++ ex2/src/acquire_daylight_hours.R
@@ -16,6 +16,10 @@ yday(date + years(1) - days(1)) -> days_in_year
 # For each day, do the following.
 for(x in 1:days_in_year){ 
   print(date)
+    #  RM
+  #   2022.06.07 summer # 1970-01-01T00:00:01+00:00
+  #   2022.12.12 winter # 1970-01-01T00:00:00+00:00
+  date <- "2022-12-07"
   url <- paste0("https://api.sunrise-sunset.org/json?lat=69.6489&lng=18.95508&date=", date, "&formatted=0")
   
   # Call API
@@ -26,18 +30,25 @@ for(x in 1:days_in_year){
     
     # transform ouput (efficiently)
     data.frame(call$results) |>
-      mutate(across(c(1:3,5:10), ymd_hms),
-             date = date,
-             is_dst = dst(date),
-             week_day = wday(date),
-             month_day = mday(date),
-             year_day = yday(date),
-             daylight_seconds = day_length,
-             daylight_minutes = day_length/60,
-             daylight_hours = day_length/60/60) |>
+      mutate(
+        # fix weird coding
+        across(everything(), ~ ifelse(.x=="1970-01-01T00:00:01+00:00",
+                                         paste(date,"23:59:59"),
+                                         ifelse(.x=="1970-01-01T00:00:00+00:00",
+                                                paste(date,"00:00:01"),
+                                                .x))),
+        across(c(1:3,5:10), ymd_hms), # make date
+        date = date,    
+        is_dst = dst(date),
+        week_day = wday(date),
+        month_day = mday(date),
+        year_day = yday(date),
+        daylight_seconds = day_length,
+        daylight_minutes = day_length/60,
+        daylight_hours = day_length/60/60) |> 
       select(date, year_day, month_day, week_day, 1:2, daylight_seconds, 
              daylight_minutes, daylight_hours, is_dst, 
-             everything(), -day_length) |> 
+             everything(), -day_length) |>
       rbind(daylight_df) -> daylight_df
       
     # Increment "date" by one day
@@ -61,7 +72,24 @@ daylight_df |> arrange(year_day) -> daylight_df
 # CONTINUE HERE Fix DF  
 #
 
+
+daylight_df |> 
+  filter(sunrise==hms("00:00:01"))
+  mutate
+
 daylight_df[daylight_df$sunrise==hms("00:00:01"),] 
+
+# Change
+ymd_hms(daylight_df$sunrise[daylight_df$sunrise==hms("00:00:01")])
+# acquire date
+  # daylight_df$date[daylight_df$sunrise==hms("00:00:01")]
+ymd_hms(paste0(daylight_df$date[daylight_df$sunrise==hms("00:00:01")], " 23:59:59"))
+
+ymd_hms("2020-05-17 00:00:01")
+
+
+
+
 t$sunrise=="00:00:01"
 
 
