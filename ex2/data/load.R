2f7c4e1d269ac206d3ad152a29c619093b86a7f8
Steffen Aasen <98240487+a-asen@users.noreply.github.com>
2023-10-04 18:36:45 +0200

    rm files

--- ex2/data/load.R
+++
@@ -1,7 +0,0 @@
-load <- \(path, f_pattern = ".rdata", full_name = T){
-  list.files(path, pattern = f_pattern, full.names = full_name) -> fnames
-  for(file in fnames){
-    print(file)
-    load(file)
-  }
-}
