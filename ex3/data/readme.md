07dfd8e77376694bf4e231754947fb5c378cacfd
steff <98240487+a-asen@users.noreply.github.com>
2023-10-25 18:19:16 +0200

    preparation for ex3

---
+++ ex3/data/readme.md
@@ -0,0 +1,3 @@
+Data retrieved from https://osf.io/vjsm7/ (transcranial magnetic stimulation over the angular gyrus) and https://osf.io/txpu2/ (transcranial magnetic stimulation over the dorsolateral prefrontal cortex).
+
+Note. These repositories might be closed due to ongoing manuscript writing. 
---
+++ ex3/src/transformation.R
@@ -0,0 +1,38 @@
+# Script from
+library(Rcpp)
+sourceCpp("ex3/src/apen.cpp") # load apen conversion
+
+nback=25
+
+da_pro <- function(data){
+  data |> group_by(subj,session,block,proberound) |> do({
+    d <- .
+    ## unravel taps (keys)
+    taps=d$response[d$stimulus=="tap"]
+    taps=factor(taps, levels=c("lctrl","rctrl"), labels=c("left","right"))
+    taps=as.integer(taps)-1
+    taps=tail(taps, nback)
+    
+    iti=diff(d$time[d$stimulus=="tap"]) |> tail(nback-1) 
+    
+    probe1.resp=as.integer(d$response[d$stimulus=="probe1"])+1
+    probe2.resp=as.integer(d$response[d$stimulus=="probe2"])+1
+    probe3.resp=as.integer(d$response[d$stimulus=="probe3"])+1
+    ## return summary of it
+    data.frame(
+      probe1=probe1.resp,
+      probe2=probe2.resp,
+      probe3=probe3.resp,
+      apen=apen_int(taps, 2)[3],
+      bv=sd(iti)
+    )
+  }) |> ungroup() |>  
+    mutate(probe1=ordered(probe1, levels=1:4),
+           probe2=ordered(probe2, levels=1:4),
+           probe3=ordered(probe3, levels=1:4),
+           logapen=-log(log(2)-apen),
+           logbv=log(bv),
+           zlogapen=(logapen-mean(logapen,na.rm=T))/sd(logapen,na.rm=T),
+           zlogbv=(logbv-mean(logbv,na.rm=T))/sd(logbv,na.rm=T)) -> data.probe
+  return(data.probe)
+}
